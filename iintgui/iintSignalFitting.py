3c2e76712c40754c55b34380c3c3fd114f360fe1
Christoph Rosemann <christoph.rosemann@desy.de>
2019-06-19 15:52:16 +0200

    + work in progress with new fitting interface

--- iintgui/iintSignalFitting.py
+++ iintgui/iintSignalFitting.py
@@ -27,51 +27,29 @@ class iintSignalFitting(QtGui.QWidget):
     #~ removeIndex = QtCore.pyqtSignal(int)
     #~ guesspeak = QtCore.pyqtSignal(int)
 
-    def __init__(self, pDict, parent=None):
+    def __init__(self, pDict, modellist parent=None):
         super(iintSignalFitting, self).__init__(parent)
         uic.loadUi(getUIFile.getUIFile("fitpanel2.ui"), self)
         #~ self._hiddencblist = [self.firstModelCB, self.secondModelCB, self.thirdModelCB, self.fourthModelCB]
         #~ self._dummy = True
         #~ self._hiddenuselist = [self._dummy, self.useSecond, self.useThird, self.useFourth]
-        #~ self.setParameterDict(pDict)
+        self.setParameterDict(pDict)
         #~ self.performFitPushBtn.setDisabled(True)
         #~ self.guessMode.setDisabled(True)
-        #~ self.configureFirst.clicked.connect(self.emitfirstmodelconfig)
-        #~ self.configureSecond.clicked.connect(self.emitsecondmodelconfig)
-        #~ self.configureThird.clicked.connect(self.emitthirdmodelconfig)
-        #~ self.configureFourth.clicked.connect(self.emitfourthmodelconfig)
         #~ self.guessMode.stateChanged.connect(self.doGuessing)
         #~ self._inactive = [False, True, True, True]
-        #~ self._firstModelDict = {}
-        #~ self._secondModelDict = {}
-        #~ self._thirdModelDict = {}
-        #~ self._fourthModelDict = {}
-        #~ self.useSecond.stateChanged.connect(self._toggleSecond)
-        #~ self.useThird.stateChanged.connect(self._toggleThird)
-        #~ self.useFourth.stateChanged.connect(self._toggleFourth)
-        #~ self.firstModelCB.currentIndexChanged.connect(self._checkForGauss)
-        #~ self.guessMode.setToolTip("Check to enable guessing of fit start parameters, suited for varying peak positions.\nCurrently only implemented for single gaussian models.\nIf checked no second model can be selected.")
-        #~ self.useSecond.setToolTip("Check to activate the options on the line,\nenabling the choice of a model and its configuration.")
-        #~ self.useThird.setToolTip("Check to activate the options on the line,\nenabling the choice of a model and its configuration.")
-        #~ self.useFourth.setToolTip("Check to activate the options on the line,\nenabling the choice of a model and its configuration.")
-        #~ self.firstModelCB.setToolTip("Select a model from the drop down list.")
-        #~ self.secondModelCB.setToolTip("Select a model from the drop down list.")
-        #~ self.thirdModelCB.setToolTip("Select a model from the drop down list.")
-        #~ self.fourthModelCB.setToolTip("Select a model from the drop down list.")
-        #~ self.configureFirst.setToolTip("Click here to set the initial values for\nthe chosen model from the drop down list.")
-        #~ self.configureSecond.setToolTip("Click here to set the initial values for\nthe chosen model from the drop down list.")
-        #~ self.configureThird.setToolTip("Click here to set the initial values for\nthe chosen model from the drop down list.")
-        #~ self.configureFourth.setToolTip("Click here to set the initial values for\nthe chosen model from the drop down list.")
-        #~ self.performFitPushBtn.setToolTip("Press to run the fitting procedure. All chosen models\nmust be configured first, otherwise fitting is not possible.")
+        self.currentModelList.setToolTip("The current model by its constituents, only the name and type of function are indicated.")
+        self.modelList.setToolTip("Select a function for addition to the current fit mdel.")
+        self.addButton.setToolTip("If a function is selected in the drop down list next to this button, clicking it will add this function to the current model.")
+        self.removeButton.setToolTip("If a function is selection in the view of the current model, clicking this button will remove it from the current model.")
+        self.fitButton.setToolTip("Perform the fit with the current model. Might trigger the display of a configuration dialog if further details need to be provided.")
 
-    #~ def reset(self):
-        #~ self._firstModelDict = {}
-        #~ self._secondModelDict = {}
-        #~ self._thirdModelDict = {}
-        #~ self._fourthModelDict = {}
-        #~ self.performFitPushBtn.setDisabled(True)
-        #~ self.configureFirst.setDisabled(True)
-        #~ self.guessMode.setDisabled(True)
+    def reset(self):
+        self.currentModelList.setDisabled(True)
+        self.modelList.setDisabled(True)
+        self.addButton.setDisabled(True)
+        self.removeButton.setDisabled(True)
+        self.fitButton.setDisabled(True)
 
     #~ def activateConfiguration(self):
         #~ self.firstModelCB.setDisabled(False)
@@ -88,28 +66,24 @@ class iintSignalFitting(QtGui.QWidget):
         #~ self.configureFirst.setDisabled(True)
         #~ self.guessMode.setDisabled(True)
 
-    #~ def activateFitting(self):
-        #~ self.performFitPushBtn.setDisabled(False)
-        #~ self.firstModelCB.setDisabled(False)
-        #~ self.useSecond.setDisabled(False)
-        #~ self.useThird.setDisabled(False)
-        #~ self.useFourth.setDisabled(False)
+    def setParameterDict(self, pDict):
+        self._parDict = pDict
+
+    def activateFitting(self):
+        self.currentModelList.setDisabled(False)
+        self.modelList.setDisabled(False)
+        self.addButton.setDisabled(False)
+        self.fitButton.setDisabled(False)
+
         #~ self._checkForGauss()
 
-    #~ def deactivateFitting(self):
+    def deactivateFitting(self):
         #~ self.guessMode.setDisabled(True)
-        #~ self.performFitPushBtn.setDisabled(True)
-        #~ self.firstModelCB.setDisabled(True)
-        #~ self.configureFirst.setDisabled(True)
-        #~ self.secondModelCB.setDisabled(True)
-        #~ self.configureSecond.setDisabled(True)
-        #~ self.thirdModelCB.setDisabled(True)
-        #~ self.configureThird.setDisabled(True)
-        #~ self.fourthModelCB.setDisabled(True)
-        #~ self.configureFourth.setDisabled(True)
-        #~ self.useSecond.setDisabled(True)
-        #~ self.useThird.setDisabled(True)
-        #~ self.useFourth.setDisabled(True)
+        self.currentModelList.setDisabled(True)
+        self.modelList.setDisabled(True)
+        self.addButton.setDisabled(True)
+        self.removeButton.setDisabled(True)
+        self.fitButton.setDisabled(True)
 
     #~ def doGuessing(self, state):
         #~ if state is 2:
@@ -136,60 +110,12 @@ class iintSignalFitting(QtGui.QWidget):
         #~ else:
             #~ if( self.guessMode.isEnabled()):
                 #~ self.guessMode.setCheckState(2)
-
-    #~ def _toggleSecond(self):
-        #~ self._inactive[1] = not self._inactive[1]
-        #~ self.secondModelCB.setDisabled(self._inactive[1])
-        #~ self.configureSecond.setDisabled(self._inactive[1])
-        #~ if(self._inactive[1]):
-            #~ self.removeIndex.emit(1)
-        #~ else: 
-            #~ self.guessMode.setChecked(False)
-        #~ self._checkGuessEnabled()
-
-    #~ def _toggleThird(self):
-        #~ self._inactive[2] = not self._inactive[2]
-        #~ self.thirdModelCB.setDisabled(self._inactive[2])
-        #~ self.configureThird.setDisabled(self._inactive[2])
-        #~ if(self._inactive[2]):
-            #~ self.removeIndex.emit(2)
-        #~ else: 
-            #~ self.guessMode.setChecked(False)
-        #~ self._checkGuessEnabled()
-
-    #~ def _toggleFourth(self):
-        #~ self._inactive[3] = not self._inactive[3]
-        #~ self.fourthModelCB.setDisabled(self._inactive[3])
-        #~ self.configureFourth.setDisabled(self._inactive[3])
-        #~ if(self._inactive[3]):
-            #~ self.removeIndex.emit(3)
-        #~ else: 
-            #~ self.guessMode.setChecked(False)
-        #~ self._checkGuessEnabled()
-
+  
     #~ def _checkGuessEnabled(self):
         #~ if self._inactive[1] and self._inactive[2] and self._inactive[3]:
             #~ self.guessMode.setDisabled(False)
         #~ else:
             #~ self.guessMode.setDisabled(True)
-            
-    #~ def setParameterDict(self, pDict):
-        #~ self._parDict = pDict
-
-        #~ # set the different scenarios
-        #~ names = []
-        #~ model = pDict["model"]
-        #~ for name in sorted(model.keys()):
-            #~ names.append(model[name]['modeltype'])
-
-        #~ for i in range(len(names)):
-            #~ name = names[i]
-            #~ self._hiddencblist[i].setCurrentIndex(self._hiddencblist[i].findText(name))
-            #~ try:
-                #~ self._hiddenuselist[i].setDisabled(False)
-                #~ self._hiddenuselist[i].setCheckState(2)
-            #~ except AttributeError:
-                #~ pass
 
     #~ def passModels(self, modelDict):
         #~ self._modelnames = sorted([key for key in modelDict.keys()])
@@ -197,20 +123,3 @@ class iintSignalFitting(QtGui.QWidget):
         #~ self.secondModelCB.addItems(self._modelnames)
         #~ self.thirdModelCB.addItems(self._modelnames)
         #~ self.fourthModelCB.addItems(self._modelnames)
-
-    #~ def emitfirstmodelconfig(self):
-        #~ self.activateFitting()
-        #~ index = self.firstModelCB.currentIndex()
-        #~ self.modelcfg.emit(self._modelnames[index], 0)
-
-    #~ def emitsecondmodelconfig(self):
-        #~ index = self.secondModelCB.currentIndex()
-        #~ self.modelcfg.emit(self._modelnames[index], 1)
-
-    #~ def emitthirdmodelconfig(self):
-        #~ index = self.thirdModelCB.currentIndex()
-        #~ self.modelcfg.emit(self._modelnames[index], 2)
-
-    #~ def emitfourthmodelconfig(self):
-        #~ index = self.fourthModelCB.currentIndex()
-        #~ self.modelcfg.emit(self._modelnames[index], 3)
