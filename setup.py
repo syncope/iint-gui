005af07696eee02dd7f6865486c13cbd62a38966
Christoph Rosemann <christoph.rosemann@desy.de>
2019-11-14 16:29:07 +0100

    + finetuning of building and shipping a resource file all along

--- setup.py
+++ setup.py
@@ -1,14 +1,58 @@
 from setuptools import setup
+from setuptools.command.build_py import build_py
+import shutil
 
 from codecs import open
-from os import path
+import os
+import sys
 
-with open(path.join('.', 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join('.', 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 name = 'iint-gui'
 version = '0'
 release = '0.15.0'
+
+TOOL = "iintgui"
+QRCDIR = os.path.join(TOOL, "qrc")
+
+class toolBuild(build_py):
+    """ ui and qrc builder for python
+    """
+
+    @classmethod
+    def makeqrc(cls, qfile, path):
+        """  creates the python qrc files
+        :param qfile: qrc file name
+        :param path:  qrc file path
+        """
+        qrcfile = os.path.join(path, str(qfile) + ".qrc")
+        rccfile = os.path.join(path, str(qfile) + ".rcc")
+
+        compiled = os.system("rcc %s -o %s -binary" % (qrcfile, rccfile))
+        if compiled == 0:
+            print("Built: %s -> %s" % (qrcfile, rccfile))
+        else:
+            sys.stderr.write("Error: Cannot build  %s\n" % (rccfile))
+            sys.stderr.flush()
+
+    def run(self):
+        """ runner
+        :\brief: It is running during building
+        """
+        try:
+            qfiles = [(qfile[:-4], QRCDIR) for qfile
+                      in os.listdir(QRCDIR) if qfile.endswith('.qrc')]
+            for qrc in qfiles:
+                if not qrc[0] in (".", ".."):
+                    self.makeqrc(qrc[0], qrc[1])
+        except TypeError as e:
+            print(str(e))
+            sys.stderr.write("No .qrc files to build\n")
+            sys.stderr.flush()
+
+        build_py.run(self)
+
 
 setup(
     name='iintgui',
@@ -44,4 +88,7 @@ setup(
     include_package_data=True,
 
     scripts=['iintgui/bin/iint-gui'],
+    cmdclass={
+        "build_py": toolBuild,},
+
 )
